0c0f7829a9c3fb86b09b44ddb3c4fdcc52b8d1c7
Oscar Fernandez Alzate <oscarfdzalz@gmail.com>
2016-04-19 16:42:45 -0500

    Se crea proyecto de estilos boostrap usando gulp

---
+++ js/main.js
@@ -0,0 +1,15 @@
+jQuery = require('./jquery/jquery-1.12.0.min.js');
+affix = require('./bootstrap/affix.js');
+alert = require('./bootstrap/alert.js');
+button = require('./bootstrap/button.js');
+collapse = require('./bootstrap/collapse.js');
+carousel = require('./bootstrap/carousel.js');
+dropdown = require('./bootstrap/dropdown.js');
+modal = require('./bootstrap/modal.js');
+popover = require('./bootstrap/popover.js');
+scrollspy = require('./bootstrap/scrollspy.js');
+tab = require('./bootstrap/tab.js');
+tooltip = require('./bootstrap/tooltip.js');
+transition = require('./bootstrap/transition.js');
+
+window.jQuery = window.$ = jQuery;
